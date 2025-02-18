922c4bfbc23aea0e94d7470a730c532aaa273d15
maineimissamu <sam.chadwick.p@gmail.com>
2025-02-18 17:32:19 +0100

    Finished

--- tests/calculateArea.spec.js
+++ tests/calculateArea.spec.js
@@ -1,14 +1,39 @@
 // UNCOMMENT THE CODE BELOW TO START
 
-/*
+
 
 describe("Iteration 3", () => {
     describe("Function - calculateArea", () => {
         
         // Each `it` block represents 1 test. You can use the following as a template:
-        it("", () => {});
+        it("should be defined", () => {
+            expect(calculateArea).toBeDefined();
+        });
+
+        it("should take two numbers as arguments", () => {
+            expect(calculateArea.length).toBe(2);
+        });
+
+        it("should return the area of a rectangle (product of the two numbers)", () => {
+            expect(calculateArea(5, 10)).toEqual(50);
+            expect(calculateArea(3, 7)).toEqual(21);
+            expect(calculateArea(12, 12)).toEqual(144);
+        });
+
+        it("should return undefined if any of the arguments is not provided", () => {
+            expect(calculateArea(5)).toEqual(undefined);
+            expect(calculateArea()).toEqual(undefined);
+            expect(calculateArea(undefined, 10)).toEqual(undefined);
+        });
+
+        it("should return undefined if any of the arguments is not a number", () => {
+            expect(calculateArea("5", 10)).toEqual(undefined);
+            expect(calculateArea(5, "10")).toEqual(undefined);
+            expect(calculateArea("a", "b")).toEqual(undefined);
+            expect(calculateArea(null, 10)).toEqual(undefined);
+            expect(calculateArea(5, null)).toEqual(undefined);
+        });
 
     })    
 })
 
-*/
--- tests/divide.spec.js
+++ tests/divide.spec.js
@@ -1,14 +1,39 @@
 // UNCOMMENT THE CODE BELOW TO START
 
-/*
+
 
 describe("Iteration 2", () => {
     describe("Function - divide", () => {
         
         // Each `it` block represents 1 test. You can use the following as a template:
-        it("", () => {});
+         it("should be defined", () => {
+            expect(divide).toBeDefined();
+        });
+
+        it("should take two numbers as arguments", () => {
+            expect(divide.length).toBe(2);
+        });
+
+        it("should return the division of the two numbers", () => {
+            expect(divide(10, 2)).toEqual(5);
+            expect(divide(20, 4)).toEqual(5);
+            expect(divide(100, 10)).toEqual(10);
+        });
+
+        it("should return undefined if any of the arguments is not provided", () => {
+            expect(divide(10)).toEqual(undefined);
+            expect(divide()).toEqual(undefined);
+            expect(divide(undefined, 10)).toEqual(undefined);
+        });
+
+        it("should return undefined if any of the arguments is not a number", () => {
+            expect(divide("10", 2)).toEqual(undefined);
+            expect(divide(10, "2")).toEqual(undefined);
+            expect(divide("a", "b")).toEqual(undefined);
+            expect(divide(null, 2)).toEqual(undefined);
+            expect(divide(10, null)).toEqual(undefined);
+        });
 
     })    
 })
 
-*/
